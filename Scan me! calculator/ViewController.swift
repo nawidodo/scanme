40507defaa8fbd93f6f8ae5965999040541ef27c
Nugroho Arief Widodo <nawidodo@gmail.com>
2023-04-29 13:25:33 +0700

    Initial Commit

---
+++ Scan me! calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Scan me! calculator
+//
+//  Created by Nugroho Arief Widodo on 29/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
